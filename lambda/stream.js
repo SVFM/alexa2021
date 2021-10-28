f13cbe808e818b4b3b7b3fbec07218e77a751baa
SVFM <93352928+SVFM@users.noreply.github.com>
2021-10-28 22:48:00 +0100

    Add files via upload

---
+++ lambda/stream.js
@@ -0,0 +1,9 @@
+const stream = {
+  "token": "1",
+  "url": "https://stream4.hippynet.co.uk:8014/stream",
+  "metadata": {
+    "title": "Somer Valley FM - 2021"
+  }
+}
+
+module.exports = { stream };
